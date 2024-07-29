97f12e22829608fadba6e4cf9bcce792ce73df24
Uday Kumar <udaykumar.77348@gmail.com>
2024-07-29 18:52:01 +0530

    Updated

--- src/components/Row/row.js
+++ src/components/Row/row.js
@@ -1,26 +1,31 @@
-import React , {useEffect , useState , useRef} from 'react'
-import axios from 'axios'
+import React, { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
 import './row.css';
 import YouTube from 'react-youtube';
 import movieTrailer from 'movie-trailer';
 
 const base_url = "https://image.tmdb.org/t/p/original";
 
-const Row = ({title , fetchUrl , isLargeRow}) => {
-
-  const [movies , setMovies] = useState([])
-  const [trailerUrl , setTrailerUrl] = useState("");
+const Row = ({ title, fetchUrl, isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   const rowRef = useRef(null);
 
-  // console.log("process.env.REACT_APP_API" , process.env.REACT_APP_API)
-  useEffect(()=>{
-    axios.get(`${process.env.REACT_APP_API}${fetchUrl}`)
-    .then((res)=>{
-      setMovies(res.data.results);
-      return res;
-    })
-  },[fetchUrl])
+  useEffect(() => {
+    const url = `${process.env.REACT_APP_API}${fetchUrl}`;
+    console.log("Full API URL:", url); // Add this line
+    axios.get(url)
+      .then((res) => {
+        setMovies(res.data.results);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error); // Add this line
+        if (error.response && error.response.status === 404) {
+          alert("The requested resource was not found.");
+        }
+      });
+  }, [fetchUrl]);
 
   useEffect(() => {
     const scrollInterval = setInterval(() => {
@@ -43,52 +48,49 @@ const Row = ({title , fetchUrl , isLargeRow}) => {
     return () => clearInterval(scrollInterval);
   }, [movies]);
 
-
   console.log(movies);
 
-  const handleClick = (movie)=>{
-    if(trailerUrl)
-    {
-      setTrailerUrl("")
-    }
-    else{
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
       movieTrailer(movie?.name || " ")
-      .then((url)=>{
-        const urlParams = new URLSearchParams(new URL(url).search);
-        setTrailerUrl(urlParams.get("v"))
-      }).catch((err)=>{
-        console.log("Failed" , err)
-        alert("No video Found");
-      })
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        }).catch((err) => {
+          console.log("Failed", err);
+          alert("No video Found");
+        });
     }
   }
 
   const opts = {
-    height : "390",
-    width : "100%",
-    playerVars : {
-      autoplay : 1,
-      rel : 0,
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+      rel: 0,
     },
   };
 
   return (
-    <div className= "row">
-        <h2>{title}</h2>
-        <div className='row-posters' ref = {rowRef}>
-          {movies && movies.map((item , i)=>{
-            return <img 
-            onClick={()=>handleClick(item)}
-            key = {item.id}
+    <div className="row">
+      <h2>{title}</h2>
+      <div className='row-posters' ref={rowRef}>
+        {movies && movies.map((item, i) => (
+          <img
+            onClick={() => handleClick(item)}
+            key={item.id}
             className={`row-poster ${isLargeRow && "row-posterLarge"}`}
             src={`${base_url}${isLargeRow ? item.poster_path : item.backdrop_path}`}
-            alt = {item.name}
-            />
-          })}
-        </div>
-        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+            alt={item.name}
+          />
+        ))}
+      </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   )
 }
 
-export default Row
+export default Row;
